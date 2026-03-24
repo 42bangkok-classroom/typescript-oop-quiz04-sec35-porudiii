ac8470b71ebc2461fe22acc454dc5c5cf4bf33f7
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-24 22:29:18 +0700

    5

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 
 @Injectable()
 export class UserService {
-  private filePath = path.join(process.cwd(), 'data', 'user.json');
+  private filePath = path.join(process.cwd(), 'data', 'users.json');
 
   test(): IUser[] {
     return [];

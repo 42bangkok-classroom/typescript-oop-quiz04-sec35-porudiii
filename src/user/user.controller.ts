82a11c6d20efacef91a105c04f03b65ddf16ed7f
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-24 22:57:17 +0700

    5

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { IUser } from './user.interface';
 import { UserService } from './user.service';
 
@@ -14,5 +14,13 @@ export class UserController {
   @Get()
   findAll(): IUser[] {
     return this.userService.findAll();
+  }
+  @Get(':id')
+  findOne(
+    @Param('id') id: string,
+    @Query('fields') fields?: string,
+  ): Partial<IUser> {
+    const fieldArray = fields ? fields.split(';') : undefined;
+    return this.userService.findOne(id, fieldArray);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { IUser } from './user.interface';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -14,5 +14,30 @@ export class UserService {
   findAll(): IUser[] {
     const data = fs.readFileSync(this.filePath, 'utf-8');
     return JSON.parse(data) as IUser[];
+  }
+
+  findOne(id: string, fields?: string[]): Partial<IUser> {
+    const users = this.findAll();
+    const user = users.find((u) => u.id === +id);
+
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+    if (fields === undefined) {
+      return user;
+    }
+    if (fields.length === 0) {
+      return {};
+    }
+
+    const result: Partial<IUser> = {};
+
+    fields.forEach((field) => {
+      if (field in user) {
+        result[field as keyof IUser] = field[field as keyof IUser];
+      }
+    });
+
+    return result;
   }
 }
